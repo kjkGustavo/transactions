7c80b4be0cf9008466f14ae75febf796f8d6a691
kjkGustavo <contato@gustavolima.dev>
2022-10-15 11:30:21 -0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => (
+  <div>
+    <Head>
+      <title>Challenge</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <h1>Soon</h1>
+  </div>
+)
+
+export default Home
