fe2591acf905daaeb5b70b6a89161b684b7d102e
kjkGustavo <contato@gustavolima.dev>
2022-10-22 02:05:04 -0300

    feat(action): finished

---
+++ src/components/Action.tsx
@@ -0,0 +1,15 @@
+import NextLink, { LinkProps as NextLinkProps } from 'next/link'
+
+type LinkProps = NextLinkProps & {
+  children: React.ReactNode
+}
+
+const Action = ({ children, ...props }: LinkProps) => (
+  <NextLink {...props}>
+    <a className="bg-lime-400 hover:bg-lime-300 transition-all duration-300 font-light px-4 py-2 leading-4 rounded-md text-sm box-border">
+      {children}
+    </a>
+  </NextLink>
+)
+
+export default Action
