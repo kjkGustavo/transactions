f8e18fcdba563e4ad7ae3c461661c42aa99a7434
kjkGustavo <contato@gustavolima.dev>
2022-10-23 23:49:54 -0300

    fix(layout): fix scroll x

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import Header from './Header'
 const Layout = ({ children }: { children: React.ReactNode }) => (
   <>
     <Header />
-    <div className="font-poppins h-screen w-screen bg-gray-100 pt-24">
+    <div className="font-poppins min-h-screen h-full bg-gray-100 pt-24 pb-10">
       <main className="container mx-auto shadow-2xl bg-white border-[1px] border-black/20 px-14 py-11 rounded-2xl">
         {children}
       </main>
