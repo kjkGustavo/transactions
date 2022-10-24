dda1c0902c32e47904e64acb9b3724b2d647fb50
kjkGustavo <contato@gustavolima.dev>
2022-10-24 03:00:49 -0300

    style: border fix

--- src/components/Action.tsx
+++ src/components/Action.tsx
@@ -6,7 +6,7 @@ type LinkProps = NextLinkProps & {
 
 const Action = ({ children, ...props }: LinkProps) => (
   <NextLink {...props}>
-    <a className="bg-lime-400 border-lime-500 border-[1px] hover:bg-lime-300 transition-all duration-300 font-light px-4 py-2 leading-4 rounded-md text-sm box-border">
+    <a className="bg-lime-400 border-lime-500 border hover:bg-lime-300 transition-all duration-300 font-light px-4 py-2 leading-4 rounded-md text-sm box-border">
       {children}
     </a>
   </NextLink>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => (
   <>
     <Header />
     <div className="font-poppins min-h-screen h-full bg-gray-100 pt-24 pb-10">
-      <main className="container mx-auto shadow-2xl bg-white border-[1px] border-black/20 px-14 py-11 rounded-2xl">
+      <main className="container mx-auto shadow-2xl bg-white border border-black/20 px-14 py-11 rounded-2xl">
         {children}
       </main>
     </div>
